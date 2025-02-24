a2aa86c92ca50ed086ba0fa81fbaaba7c46a6648
Isopod <oskar.szmukier@gmail.com>
2025-02-24 16:06:47 +0100

    Adds automatic folder generation

--- Web Crawler.py
+++ Web Crawler.py
@@ -1,15 +1,32 @@
-import requests, xml.etree.ElementTree as et
+import requests, xml.etree.ElementTree as et, shutil, os
+from HtmlTagNames import html_tag_names
 
-def main():
-    # request = requests.request("GET", "https://books.toscrape.com")
-    # html = request.text
-    # with open("XMLs/toscrape.txt", "w+", encoding="UTF-8") as file:
-    #     file.write(html)
-    with open("XMLs/toscrape.txt", "r", encoding="UTF-8") as file:
-        html = "".join(file.readlines())
-    html = close_tags(html)
-    elmtree = et.ElementTree(et.fromstring(html))
-    print([x for x in elmtree.iter("p")])
+def main(): 
+    if not os.path.exists("./Downloads"):
+        os.mkdir("./Downloads")
+    if not os.path.exists("./History/"):
+        os.mkdir("./History/")
+
+    link = input("Please insert a valid link to a website: ")
+    link = verify_link(link)
+
+    request = requests.get(link)
+    html = request.text
+
+    with open("History/"+link[link.find("/")+2:-1]+".txt", "w+", encoding="UTF-8") as file:
+        file.write(html)
+
+    html = close_tags(html) # XML valid string of website
+    elmtree = et.ElementTree(et.fromstring(html)) # XML tree of website
+
+    tag = decide_tag()
+    if tag=="img":
+        sources = [x.attrib["src"] for x in elmtree.iter(tag)]
+    elif tag=="video" or tag=="audio":
+        sources = [x.find("source").attrib["src"] for x in elmtree.iter(tag)]
+    else:
+        sources = [x.te for x in elmtree.iter(tag)]
+
 
 
 def close_tags(tree: str):
@@ -17,10 +34,8 @@ def close_tags(tree: str):
     x = 0
     while x != len(tree):
         # handle escaped back slashes
-        if test == "\\":
-            test = tree[x:x+2]
-            tree = tree.replace(tree[x:x+2],tree[x]) 
-            test = tree[x:x+2]
+        if tree[x+1:x+2] == "\\":
+            tree = tree.replace(tree[x:x+2],tree[x])
         # handle comments and doctype
         if tree[x:x+2] == "<!":
             x = tree.find("-->", x)+2
@@ -55,6 +70,49 @@ def close_tags(tree: str):
             break
         x = openpos
     return tree
+def download(link: str | list[str], directory="./Downloads/"):
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+    
+    if type(link) == "list":
+        for x in link:
+            image_name = x[len(x)-x[::-1].find("/"):]
+            with open(directory+image_name, "wb") as img:
+                shutil.copyfileobj(requests.request("GET", link+x, stream=True).raw, img)
+    else:
+        image_name = x[len(x)-x[::-1].find("/"):]
+        with open(directory+image_name, "wb") as img:
+            shutil.copyfileobj(requests.request("GET", link+x, stream=True).raw, img)
+def verify_link(domain: str):
+    if domain[:7] != "http://" and domain[:8] != "https://":
+        domain = "http://"+domain
+    if domain[-1] != "/":
+        domain += "/" 
+    return domain
+def decide_tag():
+    print("Please decide which tags to download: ")
+    print("1. Images.")
+    print("2. Videos.")
+    print("3. Audios.")
+    print("4. Others.")
+    while True:
+        tag = int(input())
+        match tag:
+            case 1:
+                return "img"
+            case 2:
+                return "video"
+            case 3:
+                return "audio"
+            case 4:
+                while True:
+                    tag = input("Please specify tag: ")
+                    if tag in html_tag_names:
+                        break
+                    print("Chosen tag does not exist, please input a correct tag.")
+                return tag
+            case _:
+                print("Error. Please choose a correct number.")
 
 
 main()
